a9d78eaad23e5066745e3d26a603a651203b650a
Michael <M.Flower@gmx.de>
2017-12-10 13:40:39 +0100

    Use service-worker for offline capabilities.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { TimerModule } from './timer/timer.module';
-import { MinutesAndSecondsPipe } from './shared/pipes/minutes-and-seconds.pipe';
 import { SharedModule } from './shared/shared.module';
 import { SetupModule } from './setup/setup.module';
 import { TeaMaterialModule } from './tea-material/tea-material.module';
-import { SetupComponent } from './setup/setup.component';
 import { LicensesComponent } from './licenses/licenses.component';
 import { NotificationService } from './notification.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { TimerComponent } from './timer/timer.component';
 import { AppRoutingModule } from './app.routing.module';
+import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -25,7 +24,8 @@ import { AppRoutingModule } from './app.routing.module';
     SetupModule,
     TimerModule,
     TeaMaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
   ],
   providers: [
     NotificationService
